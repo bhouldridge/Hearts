6c6d7744e7c915f2994bc11732f6768724bc7f5f
Benjamin Houldridge <64706658+bhouldridge@users.noreply.github.com>
2021-08-25 18:51:46 -0700

    First Upload.
    Just started out, with a couple of tests, and objects. Card and Deck objects implemented with minimal functionality.

---
+++ Hearts/hearts.py
@@ -0,0 +1,42 @@
+class Card:
+    
+    face_value_dict = {'Two':2,
+                       'Three':3,
+                       'Four':4,
+                       'Five':5,
+                       'Six':6,
+                       'Seven':7,
+                       'Eight':8,
+                       'Nine':9,
+                       'Ten':10,
+                       'Jack':11,
+                       'Queen':12,
+                       'King':13,
+                       'Ace':14}
+    suits = ('Clubs', 'Hearts', 'Spades', 'Diamonds')
+    
+    def __init__(self, face, suit):
+
+        self.face = face
+        self.value = Card.face_value_dict[self.face]
+        self.suit = suit
+
+    def __repr__(self):
+        return(self.face + ' of ' + self.suit)
+    
+    
+class Deck:
+    
+    suits = ('Dimonds', 'Clubs', 'Hearts', 'Spades')
+    faces = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine',
+             'Ten', 'Jack', 'Queen', 'King', 'Ace')
+    
+    def __init__(self):
+        self.cards = []
+        for suit in self.suits:
+            for face in self.faces:
+                self.cards.append(Card(face,suit))
+    
+    def __iter__(self):
+        return iter(self.cards)
+                
